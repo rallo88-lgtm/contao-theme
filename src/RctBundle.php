958645dd4a10c6fe0c8263fc89f119fd791bf902
Ralph Engels <rallo88@googlemail.com>
2026-05-06 22:21:45 +0200

    chore: bump RctBundle::VERSION 1.6.2 -> 1.6.3
    Newsreader-Erweiterungen fuer contao-pdf-import-Magazin-Output:
    - mod_newsreader-CE-Stack + figcaption + Mobile-Padding (gestern)
    - rct-infografik Multi-Column-Gallery + max-width-Fix (gestern)
    - rct-infografik-caption als Workaround fuer gallery-Caption-Klau
    - rct-infografik BG-Color #f9f9f9 als CSS-Var

--- src/RctBundle.php
+++ src/RctBundle.php
@@ -6,7 +6,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class RctBundle extends Bundle
 {
-    public const VERSION = '1.6.2';
+    public const VERSION = '1.6.3';
 
     public function getPath(): string
     {
